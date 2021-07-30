7328d6dde88261d58baf3b5650968f7ce13242cc
Hy Che <giahy.che@shodan.io>
2021-07-30 14:04:08 +0700

    Remove CKAN mapping to CSL variables
    This requires user map their own variables by override the snippet

--- ckanext/citation/helpers.py
+++ ckanext/citation/helpers.py
@@ -2,10 +2,6 @@ from six import string_types
 from ckan.common import config, json
 
 
-CKAN_TO_CSL_FIELDS = {
-    'title': 'title',
-    'author': 'author'
-}
 
 _helpers = {}
 
@@ -19,12 +15,3 @@ def get_helpers():
     return _helpers.copy()
 
 
-@helper
-def map_ckan_to_csl_field():
-    mappings = config.get('ckanext.citation.csl_mappings')
-    if mappings:
-        if isinstance(mappings, string_types):
-            CKAN_TO_CSL_FIELDS.update(json.loads(mappings))
-        elif isinstance(mappings, dict):
-            CKAN_TO_CSL_FIELDS.update(mappings)
-    return CKAN_TO_CSL_FIELDS
